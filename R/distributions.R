cfa30893b0555e1336ec5e03b20e0e98be674389
Paul Northrop <p.northrop@ucl.ac.uk>
2017-02-24 10:23:39 +0000

    Add location parameter to dgp(), pgp(), qgp() and rgp().  Enable user to specify vector-valued parameters provided that the other parameters are vectors of the same length or of length one.

--- R/distributions.R
+++ R/distributions.R
@@ -14,6 +14,8 @@
 #'   In \code{dgev}, \code{pgev} and \code{qgev} these vectors can have
 #'   length > 1, but only if \code{length(x)[dgev]}, \code{length(q)[pgev]},
 #'   or \code{length(p)[qgev]} has length one.
+#'   Then they must either have a common length or a subset of
+#'   them must have a common length with the others having length one.
 #'   Otherwise, they must have length one.
 #' @param n Numeric scalar.  The number of observations to be simulated.
 #' @param log A logical scalar.  If TRUE the log-density is returned.
@@ -48,13 +50,13 @@
 #' @examples
 #' dgev(-1:4, 1, 0.5, 0.8)
 #' dgev(1:6, 1, 0.5, -0.2, log = TRUE)
-#' dgev(1, c(1, 2), c(1, 2), c(-0.2, 0.4))
+#' dgev(1, shape = c(-0.2, 0.4))
 #'
 #' pgev(-1:4, 1, 0.5, 0.8)
 #' pgev(1:6, 1, 0.5, -0.2)
 #' pgev(1, c(1, 2), c(1, 2), c(-0.2, 0.4))
-#' pgev(7, c(1, 2), c(1, 2), c(-0.2, 0.4))
 #' pgev(-3, c(1, 2), c(1, 2), c(-0.2, 0.4))
+#' pgev(7, 1, 1, c(-0.2, 0.4))
 #'
 #' qgev((1:9)/10, 2, 0.5, 0.8)
 #' qgev(0.5, c(1,2), c(0.5, 1), c(-0.5, 0.5))
@@ -79,10 +81,15 @@ dgev <- function (x, loc = 0, scale = 1, shape = 0,
   len_loc <- length(loc)
   len_scale <- length(scale)
   len_shape <- length(shape)
-  len_x <- length(x)
-  if (len_loc != len_scale || len_scale != len_shape) {
-    stop("loc, scale and shape must have the same length.")
+  check_len <- c(len_loc, len_scale, len_shape)
+  if (length(unique(check_len[check_len > 1])) > 1) {
+    stop("loc, scale and shape have incompatible lengths.")
   }
+  max_len <- max(check_len)
+  loc <- rep(loc, length.out = max_len)
+  scale <- rep(scale, length.out = max_len)
+  shape <- rep(shape, length.out = max_len)
+  len_x <- length(x)
   if (len_shape != 1 & len_x != 1) {
     stop("If length(x) > 1 then loc, scale and shape must have length one.")
   }
@@ -130,10 +137,15 @@ pgev <- function (q, loc = 0, scale = 1, shape = 0, lower_tail = TRUE){
   len_loc <- length(loc)
   len_scale <- length(scale)
   len_shape <- length(shape)
-  len_q <- length(q)
-  if (len_loc != len_scale || len_scale != len_shape) {
-    stop("loc, scale and shape must have the same length.")
+  check_len <- c(len_loc, len_scale, len_shape)
+  if (length(unique(check_len[check_len > 1])) > 1) {
+    stop("loc, scale and shape have incompatible lengths.")
   }
+  max_len <- max(check_len)
+  loc <- rep(loc, length.out = max_len)
+  scale <- rep(scale, length.out = max_len)
+  shape <- rep(shape, length.out = max_len)
+  len_q <- length(q)
   if (len_shape != 1 & len_q != 1) {
     stop("If length(q) > 1 then loc, scale and shape must have length one.")
   }
@@ -172,7 +184,7 @@ pgev <- function (q, loc = 0, scale = 1, shape = 0, lower_tail = TRUE){
 #' @rdname gev
 #' @export
 qgev <- function (p, loc = 0, scale = 1, shape = 0, lower_tail = TRUE) {
-  if (min(p) < 0 || max(p) > 1) {
+  if (min(p) <= 0 || max(p) >= 1) {
     stop("The elements of p must all be in (0,1)")
   }
   if (min(scale) < 0) {
@@ -181,10 +193,15 @@ qgev <- function (p, loc = 0, scale = 1, shape = 0, lower_tail = TRUE) {
   len_loc <- length(loc)
   len_scale <- length(scale)
   len_shape <- length(shape)
-  len_p <- length(p)
-  if (len_loc != len_scale || len_scale != len_shape) {
-    stop("loc, scale and shape must have the same length.")
+  check_len <- c(len_loc, len_scale, len_shape)
+  if (length(unique(check_len[check_len > 1])) > 1) {
+    stop("loc, scale and shape have incompatible lengths.")
   }
+  max_len <- max(check_len)
+  loc <- rep(loc, length.out = max_len)
+  scale <- rep(scale, length.out = max_len)
+  shape <- rep(shape, length.out = max_len)
+  len_p <- length(p)
   if (len_shape != 1 & len_p != 1) {
     stop("If length(p) > 1 then loc, scale and shape must have length one.")
   }
@@ -226,27 +243,30 @@ rgev_vec <- Vectorize(rgev, vectorize.args = c("n", "loc", "scale", "shape"))
 #' @param x,q Numeric vectors of quantiles.  All elements of \code{x}
 #'   and \code{q} must be non-negative.
 #' @param p A numeric vector of probabilities in (0,1).
-#' @param scale,shape Numeric vectors, of the same length.
-#'   Scale and shape parameters.
+#' @param loc,scale,shape Numeric vectors.
+#'   Location, scale and shape parameters.
 #'   All elements of \code{scale} must be positive.
 #'   In \code{dgp}, \code{pgp} and \code{qgp} these vectors can have
 #'   length > 1, but only if \code{length(x)[dgp]}, \code{length(q)[pgp]},
 #'   or \code{length(p)[qgp]} has length one.
+#'   Then they must either have a common length or a subset of
+#'   them must have a common length with the others having length one.
 #'   Otherwise, they must have length one.
 #' @param n Numeric scalar.  The number of observations to be simulated.
 #' @param log A logical scalar.  If TRUE the log-density is returned.
 #' @param lower_tail A logical scalar.  If TRUE (default), probabilities
 #'   are P[X <= x], otherwise, P[X > x].
 #' @details The distribution function of a GP distribution with parameters
-#'  \code{scale} = \eqn{\sigma} (>0) and \code{shape} = \eqn{\xi} is
-#'  \deqn{F(x) = 1 - [1 + \xi x / \sigma] ^ (-1/\xi) }
-#'  for \eqn{1 + \xi x / \sigma > 0}.  If \eqn{\xi = 0} the
+#'  \code{location} = \eqn{\mu}, \code{scale} = \eqn{\sigma} (>0) and
+#'  \code{shape} = \eqn{\xi} is
+#'  \deqn{F(x) = 1 - [1 + \xi (x - \mu) / \sigma] ^ (-1/\xi) }
+#'  for \eqn{1 + \xi (x - \mu) / \sigma > 0}.  If \eqn{\xi = 0} the
 #'  distribution function is defined as the limit as \eqn{\xi} tends to zero.
 #'  The support of the distribution depends on \eqn{\xi}: it is
-#'  \eqn{x >= 0} for \eqn{\xi >= 0};
-#'  and \eqn{0 <= x <= - \sigma / \xi} for \eqn{\xi < 0}.  Note that
+#'  \eqn{x >= \mu} for \eqn{\xi >= 0};
+#'  and \eqn{0 <= x <= \mu - \sigma / \xi} for \eqn{\xi < 0}.  Note that
 #'  if \eqn{\xi < -1} the GP density function becomes infinite as \eqn{x}
-#'  approaches \eqn{-\sigma/\xi}.
+#'  approaches \eqn{\mu - \sigma/\xi}.
 #'
 #'  See
 #'  \url{https://en.wikipedia.org/wiki/Generalized_Pareto_distribution}
@@ -261,23 +281,22 @@ rgev_vec <- Vectorize(rgev, vectorize.args = c("n", "loc", "scale", "shape"))
 #'   Modeling of Extreme Values}, Springer-Verlag, London.
 #'   \url{http://dx.doi.org/10.1007/978-1-4471-3675-0}
 #' @examples
-#' dgp(0:4, 0.5, 0.8)
-#' dgp(1:6, 0.5, -0.2, log = TRUE)
-#' dgp(1, c(1, 2), c(-0.2, 0.4))
+#' dgp(0:4, scale = 0.5, shape = 0.8)
+#' dgp(1:6, scale = 0.5, shape = -0.2, log = TRUE)
+#' dgp(1, scale = 1, shape = c(-0.2, 0.4))
 #'
-#' pgp(0:4, 0.5, 0.8)
-#' pgp(1:6, 0.5, -0.2)
-#' pgp(1, c(1, 2), c(-0.2, 0.4))
-#' pgp(7, c(1, 2), c(-0.2, 0.4))
-#' pgp(-3, c(1, 2), c(-0.2, 0.4))
+#' pgp(0:4, scale = 0.5, shape = 0.8)
+#' pgp(1:6, scale = 0.5, shape = -0.2)
+#' pgp(1, scale = c(1, 2), shape = c(-0.2, 0.4))
+#' pgp(7, scale = 1, shape = c(-0.2, 0.4))
 #'
-#' qgp((1:9)/10, 0.5, 0.8)
-#' qgp(0.5, c(0.5, 1), c(-0.5, 0.5))
+#' qgp((1:9)/10, scale = 0.5, shape = 0.8)
+#' qgp(0.5, scale = c(0.5, 1), shape = c(-0.5, 0.5))
 #'
 #' p <- (1:9)/10
-#' pgp(qgp(p, 1, 2, 0.8), 2, 0.8)
+#' pgp(qgp(p, scale = 2, shape = 0.8), scale = 2, shape = 0.8)
 #'
-#' rgp(6, 0.5, 0.8)
+#' rgp(6, scale = 0.5, shape = 0.8)
 #' @name gp
 NULL
 ## NULL
@@ -286,23 +305,29 @@ NULL
 
 #' @rdname gp
 #' @export
-dgp <- function (x, scale = 1, shape = 0, log = FALSE) {
-  if (any(x < 0)) {
-    stop("invalid x: x must be non-negative.")
+dgp <- function (x, loc = 0, scale = 1, shape = 0, log = FALSE) {
+  if (any(x < loc)) {
+    stop("invalid x: x must not be less than loc.")
   }
   if (min(scale) < 0) {
     stop("invalid scale: scale must be positive.")
   }
+  len_loc <- length(loc)
   len_scale <- length(scale)
   len_shape <- length(shape)
-  len_x <- length(x)
-  if (len_scale != len_shape) {
-    stop("scale and shape must have the same length.")
+  check_len <- c(len_loc, len_scale, len_shape)
+  if (length(unique(check_len[check_len > 1])) > 1) {
+    stop("loc, scale and shape have incompatible lengths.")
   }
+  max_len <- max(check_len)
+  loc <- rep(loc, length.out = max_len)
+  scale <- rep(scale, length.out = max_len)
+  shape <- rep(shape, length.out = max_len)
+  len_x <- length(x)
   if (len_shape != 1 & len_x != 1) {
     stop("If length(x) > 1 then scale and shape must have length one.")
   }
-  x <- x / scale
+  x <- (x - loc) / scale
   nn <- length(x)
   xx <- 1 + shape * x
   # co is a condition to ensure that calculations are only performed in
@@ -318,7 +343,7 @@ dgp <- function (x, scale = 1, shape = 0, log = FALSE) {
   # are linear in shape.
   if (len_x == 1) {
     shape <- shape[co]
-    d[co] <- ifelse(abs(shape) < 1e-6, -x + shape * x * (x - 2) / 2
+    d[co] <- ifelse(abs(shape) < 1e-6, -x + shape * x * (x - 2) / 2,
                     -(1 + 1 / shape) * log(xx))
   } else {
     if(abs(shape) < 1e-6) {
@@ -338,23 +363,29 @@ dgp <- function (x, scale = 1, shape = 0, log = FALSE) {
 
 #' @rdname gp
 #' @export
-pgp <- function (q, scale = 1, shape = 0, lower_tail = TRUE){
-  if (any(q < 0)) {
-    stop("invalid q: q must be non-negative.")
+pgp <- function (q, loc = 0, scale = 1, shape = 0, lower_tail = TRUE){
+  if (any(q < loc)) {
+    stop("invalid q: q must not be less than loc.")
   }
   if (min(scale) < 0) {
     stop("invalid scale: scale must be positive.")
   }
+  len_loc <- length(loc)
   len_scale <- length(scale)
   len_shape <- length(shape)
-  len_q <- length(q)
-  if (len_scale != len_shape) {
-    stop("scale and shape must have the same length.")
+  check_len <- c(len_loc, len_scale, len_shape)
+  if (length(unique(check_len[check_len > 1])) > 1) {
+    stop("loc, scale and shape have incompatible lengths.")
   }
+  max_len <- max(check_len)
+  loc <- rep(loc, length.out = max_len)
+  scale <- rep(scale, length.out = max_len)
+  shape <- rep(shape, length.out = max_len)
+  len_q <- length(q)
   if (len_shape != 1 & len_q != 1) {
     stop("If length(q) > 1 then scale and shape must have length one.")
   }
-  q <- q / scale
+  q <- (q - loc) / scale
   nn <- length(q)
   qq <- 1 + shape * q
   # co is a condition to ensure that calculations are only performed in
@@ -387,19 +418,25 @@ pgp <- function (q, scale = 1, shape = 0, lower_tail = TRUE){
 
 #' @rdname gp
 #' @export
-qgp <- function (p, scale = 1, shape = 0, lower_tail = TRUE) {
-  if (min(p) < 0 || max(p) > 1) {
+qgp <- function (p, loc = 0, scale = 1, shape = 0, lower_tail = TRUE) {
+  if (min(p) <= 0 || max(p) >= 1) {
     stop("The elements of p must all be in (0,1)")
   }
   if (min(scale) < 0) {
     stop("invalid scale: scale must be positive.")
   }
+  len_loc <- length(loc)
   len_scale <- length(scale)
   len_shape <- length(shape)
-  len_p <- length(p)
-  if (len_scale != len_shape) {
-    stop("scale and shape must have the same length.")
+  check_len <- c(len_loc, len_scale, len_shape)
+  if (length(unique(check_len[check_len > 1])) > 1) {
+    stop("loc, scale and shape have incompatible lengths.")
   }
+  max_len <- max(check_len)
+  loc <- rep(loc, length.out = max_len)
+  scale <- rep(scale, length.out = max_len)
+  shape <- rep(shape, length.out = max_len)
+  len_p <- length(p)
   if (len_shape != 1 & len_p != 1) {
     stop("If length(p) > 1 then scale and shape must have length one.")
   }
@@ -409,18 +446,18 @@ qgp <- function (p, scale = 1, shape = 0, lower_tail = TRUE) {
   # If shape is close to zero then base calculation on an approximation that
   # is linear in shape.
   mult <- box_cox_vec(x = 1 - p, lambda = -shape, poly_order = 1)
-  return(-scale * mult)
+  return(loc - scale * mult)
 }
 
 # ----------------------------- rgp ---------------------------------
 
 #' @rdname gp
 #' @export
-rgp <- function (n, scale = 1, shape = 0) {
-  if (length(scale) != 1 || length(shape) != 1) {
+rgp <- function (n, loc = 0, scale = 1, shape = 0) {
+  if (length(loc) != 1 || length(scale) != 1 || length(shape) != 1) {
     stop("loc, scale and shape must have length one.")
   }
-  return(qgp(stats::runif(n), scale = scale, shape = shape))
+  return(qgp(stats::runif(n), loc = loc, scale = scale, shape = shape))
 }
 
 # --------------------------- rgp_vec -------------------------------
