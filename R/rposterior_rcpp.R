dd33113445c3d2485cbab36302a740de262031c7
Paul Northrop <p.northrop@ucl.ac.uk>
2017-06-13 09:44:26 +0100

    New examples. [ci skip]

--- R/rposterior_rcpp.R
+++ R/rposterior_rcpp.R
@@ -208,7 +208,6 @@
 #' library(rust)
 #'
 #' # GP model
-#' data(gom)
 #' u <- quantile(gom, probs = 0.65)
 #' fp <- set_prior(prior = "flat", model = "gp", min_xi = -1)
 #' gpg <- rpost_rcpp(n = 1000, model = "gp", prior = fp, thresh = u,
@@ -223,7 +222,6 @@
 #' plot(gpg)
 #'
 #' # Binomial-GP model
-#' data(gom)
 #' u <- quantile(gom, probs = 0.65)
 #' fp <- set_prior(prior = "flat", model = "gp", min_xi = -1)
 #' bp <- set_bin_prior(prior = "jeffreys")
@@ -233,22 +231,31 @@
 #' plot(bgpg, add_pu = TRUE)
 #'
 #' # GEV model
-#' data(portpirie)
 #' mat <- diag(c(10000, 10000, 100))
 #' pn <- set_prior(prior = "norm", model = "gev", mean = c(0,0,0), cov = mat)
 #' gevp  <- rpost_rcpp(n = 1000, model = "gev", prior = pn, data = portpirie)
 #' plot(gevp)
 #'
+#' # GEV model, informative prior constructed on the probability scale
+#' pip  <- set_prior(quant = c(85,88,95), alpha = c(4,2.5,2.25,0.25),
+#'   model = "gev", prior = "prob")
+#' ox_post <- rpost_rcpp(n = n, model = "gev", prior = pip, data = oxford)
+#' plot(ox_post)
+#'
 #' # PP model
-#' data(rainfall)
-#' rthresh <- 40
 #' pf <- set_prior(prior = "flat", model = "gev", min_xi = -1)
 #' ppr <- rpost_rcpp(n = 1000, model = "pp", prior = pf, data = rainfall,
-#'   thresh = rthresh, noy = 54)
+#'   thresh = 40, noy = 54)
 #' plot(ppr)
 #'
+#' # PP model, informative prior constructed on the quantile scale
+#' piq <- set_prior(prob = 10^-(1:3), shape = c(38.9,7.1,47),
+#'                  scale = c(1.5,6.3,2.6), model = "gev", prior = "quant")
+#' rn_post <- rpost_rcpp(n = 1000, model = "pp", prior = piq, data = rainfall,
+#'                       thresh = 40, noy = 54)
+#' plot(rn_post)
+#'
 #' # OS model
-#' data(venice)
 #' mat <- diag(c(10000, 10000, 100))
 #' pv <- set_prior(prior = "norm", model = "gev", mean = c(0,0,0), cov = mat)
 #' osv <- rpost_rcpp(n = 1000, model = "os", prior = pv, data = venice)
