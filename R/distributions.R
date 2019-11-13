5ea3fb2c4e9ddf07123557d3ff5a3cdbab0712c4
Paul Northrop <p.northrop@ucl.ac.uk>
2019-11-13 16:59:48 +0000

    Calculate dgev on log scale

--- R/distributions.R
+++ R/distributions.R
@@ -111,16 +111,16 @@ dgev <- function (x, loc = 0, scale = 1, shape = 0, log = FALSE, m = 1) {
   m <- rep_len(m, max_len)
   x <- (x - loc) / scale
   xx <- 1 + shape * x
-  d <- ifelse(xx < 0 | is.infinite(x), 0,
-         ifelse(xx == 0 & shape == -1, 1,
+  d <- ifelse(xx < 0 | is.infinite(x), -Inf,
+         ifelse(xx == 0 & shape == -1, 0,
            ifelse(xx == 0 & shape < -1, Inf,
-             ifelse(xx == 0, 0,
+             ifelse(xx == 0, -Inf,
                ifelse(abs(shape) > 1e-6,
-                 xx ^ (-(1 + 1 / shape)) * exp(-m * xx ^ (-1/ shape)),
-       exp(-x + shape * x * (x - 2) / 2 - m * exp(-x + shape * x ^ 2 / 2)))))))
-  d <- d * m / scale
-  if (log) {
-    d <- log(d)
+                 -(1 + 1 / shape) * logNegNA(xx) - m * xx ^ (-1/ shape),
+      -x + shape * x * (x - 2) / 2 - m * exp(-x + shape * x ^ 2 / 2))))))
+  d <- d + log(m) - log(scale)
+  if (!log) {
+    d <- exp(d)
   }
   return(d)
 }
