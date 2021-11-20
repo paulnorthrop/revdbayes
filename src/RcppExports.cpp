5767f8eb32205d8b1dda7a497ee90e5a8a57ed7c
Paul Northrop <p.northrop@ucl.ac.uk>
2021-11-20 11:33:39 +0000

    Tidy

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -9,6 +9,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // any_nonpos
 bool any_nonpos(const Rcpp::NumericVector& x);
 static SEXP _revdbayes_any_nonpos_try(SEXP xSEXP) {
